41e7dae2317dc8a7054fb8f62ff4aac75d4eff70
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-02-23 06:00:41 -0300

    Displaying Fetched Users

--- src/app/component/users/users.component.ts
+++ src/app/component/users/users.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+
+  constructor(private userService: UserService) {}
+  
+  ngOnInit(): void {
+    this.userService.getUsers(15).subscribe(
+      (results: any) => {
+        console.log(results)
+      }
+    );
+  }
 
 }
