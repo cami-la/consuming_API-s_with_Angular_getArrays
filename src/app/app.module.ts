43993ca93ae0ea2303fd50b10e26ed88eeb6046f
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-02-23 05:48:05 -0300

    User Service - Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { UserdetailComponent } from './component/userdetail/userdetail.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,9 +8,15 @@ import { Injectable } from '@angular/core';
 export class UserService {
   private readonly apiUrl: string = 'https://randomuser.me/api'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   //fetch users
+  public getUsers(size: number = 10): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/?results=${size}`)
+  }
 
   //fetch one user using the UUID
+  public getUser(uuid: number = 1): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/?uuid=${uuid}`)
+  }
 }
