d6e88006141cc32ff82e889330993ee7feee3648
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2023-02-23 05:41:53 -0300

    User Service - Part 1

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private readonly apiUrl: string = 'https://randomuser.me/api'
+
+  constructor() { }
+
+  //fetch users
+
+  //fetch one user using the UUID
+}
